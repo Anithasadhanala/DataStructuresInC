2ad3eca56db7d28170a00832de52064a98b8d92d
Anithasadhanala <84316640+Anithasadhanala@users.noreply.github.com>
2021-06-10 21:50:57 +0530

    Update fibonacci_search.c

--- fibonacci_search.c
+++ fibonacci_search.c
@@ -17,12 +17,12 @@ int main(){
 	f1=0;
 	f2=1;
 	
-	while(fs<size){											//f(k) in actual fibonacci
+	while(fs<size){									//f(k) in actual fibonacci
 		fs=f1+f2;
 		f1=f2;
 		f2=fs;
 	}
-	offset=-1;												//offset initialising
+	offset=-1;									//offset initialising
 	while(fs>1){
 		
 		a=finding_min(offset+f1,size-1);					//a is very important***
@@ -31,13 +31,13 @@ int main(){
 			m=1;
 			break;
 		}
-		else if (search>arr[a]){							//search>f(k) then (offset=a) and (k=k-1)
+		else if (search>arr[a]){						//search>f(k) then (offset=a) and (k=k-1)
 			offset=a;
-			fs=f2;
+			fs=f2;	
 			f2=f1;
 			f1=fs-f2;
 		}
-		else if (search<arr[a]){							//search<f(k)  then (k=k-2)
+		else if (search<arr[a]){						//search<f(k)  then (k=k-2)
 			fs=f1;
 			f2=f2-f1;
 			f1=fs-f2;
@@ -45,9 +45,9 @@ int main(){
 	}
 	if(f2 && arr[offset+1]==search){						 
 		if(m==1){
-			pass;											 //here we get two outputs if the search is not the last so,m is used**** inorder to skip repetition
+			pass;								//here we get two outputs if the search is not the last so,m is used**** inorder to skip repetition
 		}
-		else{											     // if the search is at the last index then the above steps will not give the answer
+		else{									 // if the search is at the last index then the above steps will not give the answer
 			printf("Index of search is: %d",offset+1);
 			m=1;
 		}
