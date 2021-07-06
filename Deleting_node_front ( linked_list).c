2801070a7d81dc06767926d1bbb51eff01ce0155
Anithasadhanala <84316640+Anithasadhanala@users.noreply.github.com>
2021-07-06 15:36:40 +0530

    Add files via upload

---
+++ Deleting_node_front ( linked_list).c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Nodes{
+	int data;
+	struct Nodes *link;									//creating structure of a node
+};
+typedef struct Nodes std;
+
+std *creating_more_nodes(std *head,int no_nodes){		// adding the nodes
+	int number,i;
+	
+	std *ptr;											
+	ptr=head;
+	
+	for(i=0;i<no_nodes-1;i++){
+		
+		scanf("%d",&number);
+		
+		std *current=malloc(sizeof(std));		
+		current->data=number;
+		current->link=NULL;
+		
+		ptr->link=current;
+		ptr=ptr->link;							
+	}
+	return head;
+}
+
+std *deleting_node_at_front(std *head){				//*****function definition of deleting front node*****
+	std *ptr=head;
+	head=head->link;
+	free(ptr);
+	ptr=NULL;
+	
+	return head;
+}
+
+int main(){
+	int number,i,no_nodes;
+	printf("Enter no_nodes: \n");
+	scanf("%d",&no_nodes);
+	printf("Enter elements: \n");
+	scanf("%d",&number);
+	
+	std *head=malloc(sizeof(std));							//creating head node
+	head->data=number;
+	head->link=NULL;
+	
+	if(no_nodes>1){											
+		head=creating_more_nodes(head,no_nodes);
+	}
+	printf("before deleting the element: \n");
+	
+	std*ptr=head;
+	while(ptr!=NULL){								
+		printf("%d ",ptr->data);
+		ptr=ptr->link;
+	}
+	printf("\n");
+	
+	head=deleting_node_at_front(head);   //*******calling the function of deleting a node *******
+	
+	printf("after deleting node: ");
+	
+	ptr=head;
+	while(ptr!=NULL){								
+		printf("%d ",ptr->data);
+		ptr=ptr->link;
+	}
+}
+
